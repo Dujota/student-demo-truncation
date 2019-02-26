5ca3e1d7d8fc31fdded1e5f518462d48d570a851
Denis <ddujota@internationalsafety.com>
2019-02-26 18:36:37 -0500

    added the event listener and click handler

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var viewMoreLink = document.querySelector('#view-more');
+
+viewMoreLink.addEventListener('click', function() {
+  var contentContainer = document.querySelector('.container');
+
+  contentContainer.style.height = 'auto';
+});
